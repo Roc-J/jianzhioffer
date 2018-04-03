8e86d1d3c153a03b11e0a1237e693f140f561be7
Roc-J <646481338@qq.com>
2018-04-03 18:25:10 +0800

    数值的整数次方

---
+++ problem012.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# Author: qjk
+
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        return pow(long(base), exponent)
