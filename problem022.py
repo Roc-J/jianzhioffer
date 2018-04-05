963682b8faf97efe514d215dfdca5285fec5504f
Roc-J <646481338@qq.com>
2018-04-05 10:24:47 +0800

    从上到下打印二叉树

---
+++ problem022.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# Author: qjk
+
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        if not root:
+            return []
+        queue = []
+        result = []
+        queue.append(root)
+
+        while len(queue) > 0:
+            node = queue.pop(0)
+            result.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return result
