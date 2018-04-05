be95144b62bd674fe36ee1da7cbfa35e4655a595
Roc-J <646481338@qq.com>
2018-04-05 10:26:57 +0800

    二叉搜索数与双向链表

--- problem026.py
+++ problem026.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def Convert(self, pRootOfTree):
+        # write code here
+        if not pRootOfTree:
+            return None
+
+        self.arr = []
+        self.midTravel(pRootOfTree)
+        for i, v in enumerate(self.arr[:-1]):
+            v.right = self.arr[i+1]
+            self.arr[i+1].left = v
+        return self.arr[0]
+
+
+    def midTravel(self, root):
+        if not root:
+            return
+        self.midTravel(root.left)
+        self.arr.append(root)
+        self.midTravel(root.right)
