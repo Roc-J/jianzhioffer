fd129ada8f63a0696d465a46ac56589779c97f77
Roc-J <646481338@qq.com>
2018-04-06 00:36:16 +0800

    第一次只出现一次的字符

--- problem034.py
+++ problem034.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def FirstNotRepeatingChar(self, s):
+        # write code here
+        if not s:
+            return -1
+        d = dict()
+        for item in s:
+            if item not in d:
+                d[item] = 1
+            else:
+                d[item] += 1
+
+        for i in range(len(s)):
+            if d[s[i]] == 1:
+                return i
+        return -1
+
+if __name__ == '__main__':
+    print(Solution().FirstNotRepeatingChar('googgle'))
