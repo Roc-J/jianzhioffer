20fc0252fd973addc0b2d47b1daaf775eeca8bb6
Roc-J <646481338@qq.com>
2018-04-06 20:55:12 +0800

    求1+2+...+n

--- problem047.py
+++ problem047.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def Sum_Solution(self, n):
+        # write code here
+        ans = n
+        return ans and self.Sum_Solution(n-1)+n
