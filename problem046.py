c9c592badf93455e2853618c4f1e9de9d3d2c8c3
Roc-J <646481338@qq.com>
2018-04-06 20:54:56 +0800

    孩子们的游戏

--- problem046.py
+++ problem046.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def LastRemaining_Solution(self, n, m):
+        # write code here
+        if not m or not n:
+            return -1
+        numbers = range(n)
+        i = 0
+        while len(numbers)>1:
+            i = (m-1 + i) % len(numbers)
+            numbers.pop(i)
+        return numbers[-1]
+
+if __name__ == '__main__':
+    print(Solution().LastRemaining_Solution(5,2))
+
