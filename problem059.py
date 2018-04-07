2e6e76ab4dc2c69891b2138386a02e67429a26ad
Roc-J <646481338@qq.com>
2018-04-07 15:02:51 +0800

    按之字形顺序打印二叉树

--- problem059.py
+++ problem059.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def Print(self, pRoot):
+        # write code here
+        if not pRoot:
+            return []
+        nodeStack = [pRoot]
+        result = []
+        while nodeStack:
+            res = []
+            nextStack = []
+            for node in nodeStack:
+                res.append(node.val)
+                if node.left:
+                    nextStack.append(node.left)
+                if node.right:
+                    nextStack.append(node.right)
+            nodeStack = nextStack
+            result.append(res)
+        returnResult = []
+        for i, v in enumerate(result):
+            if i%2 == 0:
+                returnResult.append(v)
+            else:
+                returnResult.append(v[::-1])
+        return returnResult
