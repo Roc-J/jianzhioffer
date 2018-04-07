095599bc40784d4004ff7b34fc1b4368725377b0
Roc-J <646481338@qq.com>
2018-04-07 15:01:34 +0800

    二叉树的下一个节点

--- problem057.py
+++ problem057.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+class Solution:
+    def GetNext(self, pNode):
+        # write code here
+        if not pNode:
+            return None
+        if pNode.right:
+            l = pNode.right
+            while l.left:
+                l = l.left
+            return l
+        while pNode.next:
+            temp = pNode.next
+            if temp.left == pNode:
+                return temp
+            pNode = temp
