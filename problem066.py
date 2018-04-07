0d52a08a52a2d6283a71569cbfc307cf57e54db2
Roc-J <646481338@qq.com>
2018-04-07 15:05:35 +0800

    机器人的运动范围

--- problem066.py
+++ problem066.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def __init__(self):
+        self.visit = {}
+
+    def movingCount(self, threshold, rows, cols):
+        # write code here
+        return self.moving(threshold, rows, cols, 0, 0)
+
+    def moving(self, threshold, rows, cols, row, col):
+        if row >= rows or col >= cols or row < 0 or col < 0:
+            return 0
+        if (row, col) in self.visit:
+            return 0
+        if sum(map(int, str(row) + str(col))) > threshold:
+            return 0
+
+        self.visit[(row, col)] = 1
+
+        return 1 + self.moving(threshold, rows, cols, row-1, col) + self.moving(threshold, rows, cols, row+1, col) + self.moving(threshold, rows, cols, row, col-1) + self.moving(threshold, rows, cols, row, col+1)
