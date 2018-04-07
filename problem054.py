ffa5851bff0a05b50503ff26adaedf70cfdb89e7
Roc-J <646481338@qq.com>
2018-04-07 15:00:36 +0800

    字符流中第一个不重复的字符

--- problem054.py
+++ problem054.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.s = ''
+        self.d = dict()
+
+    def FirstAppearingOnce(self):
+        # write code here
+        d = dict()
+        for i in self.s:
+            if self.d[i] == 1:
+                return i
+        return '#'
+
+    def Insert(self, char):
+        # write code here
+        self.s += char
+        if char not in self.d:
+            self.d[char] = 1
+        else:
+            self.d[char] += 1
