acc9a8e74e559215d973762aed26cb065157a686
Roc-J <646481338@qq.com>
2018-04-07 15:04:27 +0800

    数据流中的中位数

--- problem063.py
+++ problem063.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def __init__(self):
+        self.a = []
+
+    def Insert(self, num):
+        # write code here
+        self.a.append(num)
+
+    def GetMedian(self):
+        # write code here
+        self.a.sort()
+        if len(self.a)%2 == 1:
+            return self.a[len(self.a)/2]
+        else:
+            return (self.a[len(self.a)/2-1] + self.a[len(self.a)/2])/2.0
