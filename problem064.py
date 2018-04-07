474db9ed47b73e938bc5b738423302d1b4598bcc
Roc-J <646481338@qq.com>
2018-04-07 15:04:45 +0800

    滑动窗口中的最大值

--- problem064.py
+++ problem064.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution:
+    def maxInWindows(self, num, size):
+        # write code here
+        if not num or size==0 or size>len(num):
+            return []
+        queue = []
+        for i in range(size):
+            queue.append(num.pop(0))
+        result = []
+        result.append(max(queue))
+        while num:
+            queue.append(num.pop(0))
+            queue.pop(0)
+            result.append(max(queue))
+        return result
