eef60a84e167af6a0cd0e4181584ba452ed26b44
Roc-J <646481338@qq.com>
2018-04-07 15:01:17 +0800

    删除链表中重复的节点

--- problem056.py
+++ problem056.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+'''
+删除链表中重复的节点
+递归
+'''
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def deleteDuplication(self, pHead):
+        # write code here
+        if pHead is None or pHead.next is None:
+            return pHead
+        p = pHead.next
+        if p.val != pHead.val:
+            pHead.next = self.deleteDuplication(pHead.next)
+        else:
+
+            while pHead.val == p.val and p.next is not None:
+                p = p.next
+            if p.val != pHead.val:
+                pHead = self.deleteDuplication(p)
+            else:
+                return None
+        return pHead
